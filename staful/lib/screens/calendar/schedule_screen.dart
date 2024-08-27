0ee25b1946d89d1c4a31c692a25423a5947c3442
dongchan <dc.shin@okestro.com>
2024-08-27 18:21:00 +0900

    0827 schedule table

--- staful/lib/screens/calendar/schedule_screen.dart
+++ staful/lib/screens/calendar/schedule_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:staful/layouts/calendar_screen_layout.dart';
 import 'package:staful/screens/calendar/edit_schedule_screen.dart';
+import 'package:staful/widgets/merged_table_widget.dart';
+import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
 
 class ScheduleScreen extends StatefulWidget {
   final DateTime date;
@@ -21,17 +23,19 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
     final [year, month, day] = widget.date.toString().split(" ")[0].split("-");
 
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: NavigateBackAppBar(context),
-      body: Center(
-        child: GestureDetector(
-            onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const EditScheduleScreen(),
-                  ),
-                ),
-            child: Text("${widget.date.toString().split(" ")[0]} 스케줄!!!")),
-      ),
-    );
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: NavigateBackAppBar(context),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Text(day),
+                Text([year, month, day].join("."))
+              ],
+            ),
+            const Flexible(child: MergedTableWidget()),
+            // const TableViewCell(child: Text("hi"))
+          ],
+        ));
   }
 }
